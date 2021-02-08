2c67a891c9254ee853a7d87cc0bace1b605cd44d
Nazim-hasan <nazimhasan124073@gmail.com>
2021-02-08 12:58:17 +0600

    PHP Intro LabTask3

---
+++ LabTask3/Problem2.php
@@ -0,0 +1,13 @@
+<?php
+$marks=85;
+if($marks>=90)
+echo "A+";
+else if($marks>=80 && $marks<90)
+echo "A";
+else if($marks>=70 && $marks<80)
+echo "B";
+else if($marks>=60 && $marks<70)
+echo "C";
+else
+echo "F";
+?>
---
+++ LabTask3/Problem3.php
@@ -0,0 +1,15 @@
+<?php
+$length=25.45;
+$width=55.45;
+if($length==$width){
+echo "The shape is a square";}
+else
+{
+$Perimeter;
+$Area;
+$Perimeter=2*($length+$width);
+$Area=$length*$width;
+echo "Perimeter is: $Perimeter"."<br/>";
+echo "Area is: $Area";
+}
+?>
